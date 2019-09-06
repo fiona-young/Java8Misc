192ff1f29f57130e988ab9ea295cc70db98c5f05
Fiona Matters <Fiona.Matters@avaloq.com>
2019-09-06 16:49:55 +0100

    Argh

--- src/main/java/hackerrank/prep/hashMaps/CountTriplets.java
+++ src/main/java/hackerrank/prep/hashMaps/CountTriplets.java
@@ -8,6 +8,53 @@ import java.util.Map;
 public class CountTriplets {
   static long countTriplets(List<Long> arr, long r) {
     Map<Long, List<Integer>> progressionMap = new HashMap<>();
+    for (int i = 0; i < arr.size(); i++) {
+      progressionMap.putIfAbsent(arr.get(i), new ArrayList<>());
+      progressionMap.get(arr.get(i)).add(i);
+    }
+    long count = 0;
+    for (Map.Entry<Long, List<Integer>> entry : progressionMap.entrySet()) {
+      List<Integer> arr1 = entry.getValue();
+      List<Integer> arr2 = progressionMap.get(entry.getKey() * r);
+      List<Integer> arr3 = progressionMap.get(entry.getKey() * r * r);
+      if (arr2 == null || arr3 == null) {
+        continue;
+      }
+
+      long[] temp2 = new long[arr2.size()];
+      long lastVal = 0;
+      int i3 = arr3.size() - 1;
+      for (int i2 = arr2.size() - 1; i2 >= 0; i2--) {
+        while (i3 > 0 && arr3.get(i3 - 1) > arr2.get(i2)) {
+          i3--;
+        }
+        if (arr2.get(i2) < arr3.get(i3)) {
+          temp2[i2] = arr3.size() - i3 + lastVal;
+          lastVal = temp2[i2];
+        }
+      }
+
+      long[] temp1 = new long[arr1.size()];
+      lastVal = 0;
+      int i2 = temp2.length - 1;
+      for (int i1 = arr1.size() - 1; i1 >= 0; i1--) {
+        while (i2 > 0 && arr2.get(i2 - 1) > arr1.get(i1)) {
+          i2--;
+        }
+        if (arr1.get(i1) < arr2.get(i2)) {
+          temp1[i1] = temp2[i2] + lastVal;
+          lastVal = temp1[i1];
+        }
+      }
+      count += lastVal;
+    }
+
+    return count;
+  }
+}
+
+/*  static long countTriplets(List<Long> arr, long r) {
+    Map<Long, List<Integer>> progressionMap = new HashMap<>();
     for(int i = 0; i<arr.size();i++){
       progressionMap.putIfAbsent(arr.get(i), new ArrayList<>());
       progressionMap.get(arr.get(i)).add(i);
@@ -22,13 +69,13 @@ public class CountTriplets {
       }
 
       Map<Integer, Integer> map2 = new HashMap<>();
-        for (int key2 : arr2) {
-          for (int j = 0; j < arr3.size(); j++) {
-            if (key2 < arr3.get(j)) {
-              map2.put(key2, arr3.size()-j);
-              break;
-            }
+      for (int key2 : arr2) {
+        for (int j = 0; j < arr3.size(); j++) {
+          if (key2 < arr3.get(j)) {
+            map2.put(key2, arr3.size()-j);
+            break;
           }
+        }
       }
 
       for (int key1 : arr1) {
@@ -41,5 +88,5 @@ public class CountTriplets {
       }
     }
     return count;
-  }
-}
+  }*/
+
